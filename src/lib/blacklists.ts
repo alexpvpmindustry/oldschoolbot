ee25c7deb2dfc4b47116a9d916d67f57bea1300d
alexpvpmindustry <alexmindustry@gmail.com>
2022-08-27 16:41:12 +0800

    yes

--- src/lib/blacklists.ts
+++ src/lib/blacklists.ts
@@ -6,13 +6,13 @@ export const BLACKLISTED_USERS = new Set<string>();
 export const BLACKLISTED_GUILDS = new Set<string>();
 
 export async function syncBlacklists() {
-	const blacklistedEntities = await roboChimpClient.blacklistedEntity.findMany();
+	//const blacklistedEntities = await roboChimpClient.blacklistedEntity.findMany();
 	BLACKLISTED_USERS.clear();
 	BLACKLISTED_GUILDS.clear();
-	for (const entity of blacklistedEntities) {
-		const set = entity.type === 'guild' ? BLACKLISTED_GUILDS : BLACKLISTED_USERS;
-		set.add(entity.id.toString());
-	}
+	//for (const entity of blacklistedEntities) {
+	//	const set = entity.type === 'guild' ? BLACKLISTED_GUILDS : BLACKLISTED_USERS;
+	//	set.add(entity.id.toString());
+	//}
 }
 
 if (production) {
